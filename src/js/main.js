073b494feeb0966d8c5328ed04060f5fbf38052d
rexatrom <ajstylejoshua2837@gmail.com>
2025-05-15 13:00:52 +0100

    week02--individual_correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-import { ProductList } from "./ProductList.mjs";
+import ProductList from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
-
 
 const dataSource = new ProductData("tents");
 
